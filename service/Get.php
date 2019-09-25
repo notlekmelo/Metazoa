82b09fd47181140b6c036082d3b81457a23fcbea
MHenrie <silvamarlon87@gmail.com>
2019-09-25 11:33:57 -0300

    requisição axios

--- service/Get.php
+++ service/Get.php
@@ -4,63 +4,64 @@ header("Content-Type:application/json");
 if (isset($_GET['pessoa_id'])) {
     $db = new Database();
     $con = $db->connect();
-	$pessoa_id= $_GET['pessoa_id'];
-	$result = mysqli_query($con, "SELECT * FROM `pessoa` WHERE Email=$pessoa_id");
-	if(mysqli_num_rows($result)>0){
-	    $row = mysqli_fetch_array($result);
-	    $nome = $row['Nome'];
+    $pessoa_id = $_GET['pessoa_id'];
+    $result = mysqli_query($con, "SELECT * FROM `pessoa` WHERE Email=$pessoa_id");
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_array($result);
+        $nome = $row['Nome'];
         $Telefone = $row['Telefone'];
         $estado = $row['Estado'];
-        $cidade= $row['Cidade'];
-        $rua= $row['Rua'];
-        $bairro= $row['Bairro'];
+        $cidade = $row['Cidade'];
+        $rua = $row['Rua'];
+        $bairro = $row['Bairro'];
         mysqli_close($con);
-	    return response($pessoa_id, $nome,$Telefone, $estado, $cidade, $rua, $bairro);
-	}else{
-		return response("Nenhum registro encontrado");
-	}
-}else if(isset($_GET['instituicao_id'])){
+        responsePessoa($pessoa_id, $nome, $Telefone, $estado, $cidade, $rua, $bairro);
+    } else {
+        responseErro("Nenhum registro encontrado");
+    }
+} else if (isset($_GET['instituicao_id'])) {
     $db = new Database();
     $con = $db->connect();
-	$instituicao_id= $_GET['instituicao_id'];
-	$result = mysqli_query($con, "SELECT * FROM `intituicao` WHERE Email=$instituicao_id");
-	if(mysqli_num_rows($result)>0){
-	    $row = mysqli_fetch_array($result);
-	    $nome = $row['Nome'];
-	    $CNPJ = $row['CNPJ'];
+    $instituicao_id = $_GET['instituicao_id'];
+    $result = mysqli_query($con, "SELECT * FROM `intituicao` WHERE Email=$instituicao_id");
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_array($result);
+        $nome = $row['Nome'];
+        $CNPJ = $row['CNPJ'];
         $Telefone = $row['Telefone'];
         $estado = $row['Estado'];
-        $cidade= $row['Cidade'];
-        $rua= $row['Rua'];
-        $bairro= $row['Bairro'];
+        $cidade = $row['Cidade'];
+        $rua = $row['Rua'];
+        $bairro = $row['Bairro'];
         $conta = $row['Conta'];
         mysqli_close($con);
-	    return response($instituicao_id, $nome, $CNPJ,$Telefone, $estado, $cidade, $rua, $bairro,$conta);
-	}else{
-		return response("Nenhum registro encontrado");
-	}
-}
-else{
-	return response("Requisição inválida");
+        responseInstituicao($instituicao_id, $nome, $CNPJ, $Telefone, $estado, $cidade, $rua, $bairro, $conta);
+    } else {
+        responseErro("Nenhum registro encontrado");
+    }
+} else {
+    responseErro("Requisição inválida");
 }
 
-function response($pessoa_id,$Nome,$Telefone, $estado, $cidade, $rua, $bairro){
-	$response['pessoa_id'] = $pessoa_id;
-	$response['Nome'] = $Nome;
+function responsePessoa($pessoa_id, $Nome, $Telefone, $estado, $cidade, $rua, $bairro)
+{
+    $response['pessoa_id'] = $pessoa_id;
+    $response['Nome'] = $Nome;
     $response['Telefone'] = $Telefone;
     $response['Estado'] = $estado;
     $response['Cidade'] = $cidade;
     $response['Rua'] = $rua;
     $response['Bairro'] = $bairro;
 
-	$json_response = json_encode($response);
-	return $json_response;
+    $json_response = json_encode($response);
+    echo $json_response;
 }
 
-function response($instituicao_id,$Nome,$CNPJ,$Telefone, $estado, $cidade, $rua, $bairro, $conta){
-	$response['pessoa_id'] = $pessoa_id;
-	$response['Nome'] = $Nome;
-	$response['CNPJ'] = $CNPJ;
+function responseInstituicao($instituicao_id, $Nome, $CNPJ, $Telefone, $estado, $cidade, $rua, $bairro, $conta)
+{
+    $response['pessoa_id'] = $instituicao_id;
+    $response['Nome'] = $Nome;
+    $response['CNPJ'] = $CNPJ;
     $response['Telefone'] = $Telefone;
     $response['Estado'] = $estado;
     $response['Cidade'] = $cidade;
@@ -68,13 +69,13 @@ function response($instituicao_id,$Nome,$CNPJ,$Telefone, $estado, $cidade, $rua,
     $response['Bairro'] = $bairro;
     $response['Conta'] = $conta;
 
-	$json_response = json_encode($response);
-	return $json_response;
+    $json_response = json_encode($response);
+    echo $json_response;
 }
 
-function response($erro){
+function responseErro($erro)
+{
     $response['Erro'] = $erro;
     $json_response = json_encode($response);
-	return $json_response;
+    echo $json_response;
 }
-?>
